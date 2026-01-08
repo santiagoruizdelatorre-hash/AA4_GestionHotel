e46a6c731f81580a27995a1d3255d49d13f967b9
daracardenass <dara.cardenas@estudiant.enti.cat>
2026-01-08 23:11:48 +0100

    Add files via upload

---
+++ Hotel.java
@@ -0,0 +1,14 @@
+
+public class Hotel {
+    private String nombre;
+    private Habitacion[] habitaciones;
+    private Reserva[] reservas;
+
+    public void agregarHabitacion(Habitacion h){
+
+    }
+    public void crearReserva(String nombre, String dni, double telefono, LocalDate salida, String tipo) {
+
+    }
+    public buscarReservaPorId(LocalDate)
+}
