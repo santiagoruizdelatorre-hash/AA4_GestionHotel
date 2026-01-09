2a0397ce65ab635c0dc6e7788dcf8e2050c500b1
daracardenass <dara.cardenas@estudiant.enti.cat>
2026-01-09 23:27:00 +0100

    Hotel.java
    Base inicial de las clases principales , solo es un borrador de las funciones principales que tendra la clase hotel y los atributos.

--- Hotel.java
+++ Hotel.java
@@ -1,14 +1,29 @@
-
+import java.time.LocalDate;
+import java.util.Scanner;
 public class Hotel {
     private String nombre;
     private Habitacion[] habitaciones;
     private Reserva[] reservas;
 
+   
     public void agregarHabitacion(Habitacion h){
 
     }
     public void crearReserva(String nombre, String dni, double telefono, LocalDate salida, String tipo) {
 
     }
-    public buscarReservaPorId(LocalDate)
+    public Reserva buscarReservaPorId(String id){
+        return null;
+    }
+    public Reserva comprobarReserva(LocalDate dia, String id){
+        return null;
+    }
+    public Reserva cancelarReserva(String id){
+        return null;
+
+    }
+    public Ticket generarTicket(String idReserva){
+        return null;
+
+    }
 }
