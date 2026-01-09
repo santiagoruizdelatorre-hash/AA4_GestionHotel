3e39541f11187e1f87e0bc36726ca4d217b8735a
daracardenass <dara.cardenas@estudiant.enti.cat>
2026-01-09 23:31:31 +0100

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -1,3 +1,39 @@
+import java.util.Date;
+import java.util.Scanner;
+
 public class Reserva{
-    
+    private String id;
+    private Cliente cliente;
+    private Habitacion habitacion;
+    private Date fechaEntrada;
+    private Date fechaSalida;
+    private EstadoReserva estado;
+    private Scanner sc;
+
+    public Reserva(){
+        this.sc = new Scanner(System.in);
+        this.cliente = new Cliente();
+    }
+
+    public String getId(){
+        return id;
+    }
+    public int getNoches(){
+        long diferencia = fechaSalida.getTime() - fechaEntrada.getTime();
+        return (int) (diferencia/ (100 *60 *60 * 34));
+
+    }
+    public double  calcularTotal(){
+
+        }
+    }
+    public void confirmar(){
+        estado = EstadoReserva.CONFIRMADA;
+
+    }
+    public void cancelar(){
+        estado = EstadoReserva.CANCELADA;
+    }
 }
+
+
