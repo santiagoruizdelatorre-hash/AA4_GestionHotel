f99dd393fa84a8aef46651a9bfd9ef74e274ee31
daracardenass <dara.cardenas@estudiant.enti.cat>
2026-01-17 19:40:58 +0100

    Update Habitacion.java

--- Habitacion.java
+++ Habitacion.java
@@ -1,3 +1,28 @@
 public class Habitacion{
-    
+    private int numero;
+    private double precioNoche;
+    private EstadoHabitacion estado;
+
+    public Habitacion(int numero, double precioNoche) {
+        this.numero = numero;
+        this.precioNoche = precioNoche;
+        this.estado = EstadoHabitacion.DISPONIBLE;
+    }  
+
+    public double getPrecioNoche() {
+        return precioNoche;
+    }
+
+    public EstadoHabitacion getEstado() {
+        return estado;
+    }
+
+    public void ocupar() {
+        estado = EstadoHabitacion.OCUPADA;
+    }
+
+    public void liberar() {
+        estado = EstadoHabitacion.DISPONIBLE;
+    }
+
 }
