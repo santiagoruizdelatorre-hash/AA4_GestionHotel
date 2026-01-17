2c690a6ce20a49f729ad1a92d1ba3e8afcc474de
santiagoruizdelatorre-hash <santiago.ruizdelatorre@estudiant.enti.cat>
2026-01-17 10:15:39 +0100

    Update Cliente.java
    HE avanzado la clase cliente y le he añadido un constructor y unos getters.

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,26 @@
 public class Cliente {
-    
+
+    private String dni;
+    private String nombre;
+    private String telefono;
+
+    public Cliente() {
+    this.dni = "";
+    this.nombre = "";
+    this.telefono = "";
+    }
+
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
 }
+
