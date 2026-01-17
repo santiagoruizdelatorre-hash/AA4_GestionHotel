a8a6995b8c47aa82986677b5ddf0845bebd24306
daracardenass <dara.cardenas@estudiant.enti.cat>
2026-01-17 19:42:01 +0100

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,13 @@
 public class Cliente {
-    
+    private String nombre;
+    private String dni;
+
+    public Cliente(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
 }
+
